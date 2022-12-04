b2e891502d0853ff1e6583a5743992e82679ab53
Aisling Dwyer <x21123748@student.ncirl.ie>
2022-12-04 16:12:20 +0000

    Added to BloodGlucoseMonitoringService

--- BloodGlucoseMonitoringService/client/app.js
+++ BloodGlucoseMonitoringService/client/app.js
@@ -5,18 +5,16 @@ var PROTO_PATH = __dirname + "/protos/blood_glucose_monitoring.proto"
 
 var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var blood_glucose_monitoring_proto = grpc.loadPackageDefinition(packageDefinition).blood_glucose_monitoring
-var client = new blood_glucose_monitoring_proto.BloodGlucoseMonitoringService("0.0.0.0:40000", grpc.credentials.createInsecure())
+var client = new blood_glucose_monitoring_proto.BloodGlucoseMonitoringService("0.0.0.0:40001", grpc.credentials.createInsecure())
 
-var call = client.checkGlucoseLevels({});
+var bloodGlucose = readLineSync.question("What is your current blood glucose level?")
 
-call.on('data', function(response) {
-  console.log()
-})
-
-call.on('end', function() {
-
-})
-
-call.on('error', function(e) {
-  console.log(e)
-})
+try{
+  client.add({bloodGlucose: bloodGlucose}, function(error, response) {
+    try{
+      console.log(response.message)
+    } catch(e) {
+      console.log("An error occurred.")
+    }
+  })
+}
--- BloodGlucoseMonitoringService/server/app.js
+++ BloodGlucoseMonitoringService/server/app.js
@@ -7,11 +7,32 @@ var blood_glucose_monitoring_proto = grpc.loadPackageDefinition(packageDefinitio
 
 
 function checkGlucoseLevels(call, callback) {
-
+  try {
+    var bloodGlucose = parseInt(call.request.bloodGlucose)
+    if(!isNaN(bloodGlucose)) {
+      if(bloodGlucose < 4) {
+        callback(null, {
+          message: "Your blood glucose level is low. Eat 15g of carbs such as half a cup of orange juice. Check your blood glucose levels again in 15 minutes"
+        })
+      } else if(bloodGlucose > 10) {
+        callback(null, {
+          message: "Your blood glucose levels are high. You may need to take insulin."
+        })
+        } else {
+          callback(null, {
+            message: "Your blood glucose levels are normal."
+            })
+          }
+      }
+    } catch(e) {
+      callback(null, {
+        message: "An error occurred."
+      })
+    }
 }
 
 var server = new grpc.Server()
 server.addService(blood_glucose_monitoring_proto.BloodGlucoseMonitoringService.service, { checkGlucoseLevels: checkGlucoseLevels})
-server.bindAsync("0:0:0:0:40000", grpc.ServerCredentials.createInsecure(), function() {
+server.bindAsync("0:0:0:0:40001", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
